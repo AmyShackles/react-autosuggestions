26974a58a668a4174252eaa9bcc8a94253614483
Amy Shackles <amyshackles@gmail.com>
2021-01-06 15:31:08 -0800

    Adds style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: { 
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-transform-runtime"]
+    mode: "production",
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/plugin-transform-runtime"]
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "AutoSuggest.js",
+        library: "react-autosuggestions",
+        libraryTarget: "umd",
+        environment: {
+            arrowFunction: true,
+            const: true,
+            destructuring: true,
+            module: true
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ["css-loader"]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "AutoSuggest.js",
-    library: "react-autosuggestions",
-    libraryTarget: "umd",
-    environment: {
-      arrowFunction: true,
-      const: true,
-      destructuring: true,
-      module: true
+    },
+    externals: {
+        react: "react",
+        "react-dom": "reactDOM"
     }
-  },
-  externals: {
-    react: "react",
-    'react-dom': 'reactDOM'
-  }
 };
